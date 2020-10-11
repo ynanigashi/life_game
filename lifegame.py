f0e3525ee69153cec7d9ad62bd3d265d2b813fbc
misebox <misebox@gmail.com>
2020-10-11 14:18:23 +0900

    枠

--- lifegame.py
+++ lifegame.py
@@ -18,7 +18,8 @@ class Game:
     direction = 1
 
     border = 1
-    bg_color = (30, 30, 30)
+    bg_color = (230, 230, 230)
+    fg_color = (30, 30, 30)
     cell_size = 40
 
     def __init__(self):
@@ -39,19 +40,16 @@ class Game:
             px + self.border, py + self.border,
             self.cell_size-self.border, self.cell_size-self.border))
 
-
     def display(self):
         progress = (self.msec % self.interval) / self.interval
         # clear
         self.screen.fill(self.bg_color)
         # drawing
-        for x in range(0, cell_rows):
-            for y in range(0, 1):
-                self._draw_cell((30, 30, 30), x, y)
-        # update
+        pygame.draw.rect(self.screen, self.fg_color, (
+            2, 2,
+            476, 836))
         pygame.display.flip()
         return self.clock.tick(120)
-
 
     def gameover(self):
         self.__init__()
